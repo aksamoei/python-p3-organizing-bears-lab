01426f893a36aaaba2bf6866452a36195357d9af
Abraham <abukip344@gmail.com>
2024-05-31 00:16:51 -0400

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 2, "M", "Black", "Conscientiousness", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 10, "M", "Brown", "Agreeableness", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 5, "F", "Pink", "Choleric", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 7, "M", "Black", "Sanguine", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 1, "F", "Red", "Melancholic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 5, "M", "Brown", "Melancholic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 20, "F", "Purple", "Neuroticism", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 4, "F", "Blue", "Neuroticism", 1);
